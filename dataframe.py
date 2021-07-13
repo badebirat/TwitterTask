592c012df2b23dd704ecd970856ae51c4e883c3e
birat <birat@fusemachines.com>
2021-07-13 13:19:02 +0545

    Write output as csv files

--- dataframe.py
+++ dataframe.py
@@ -9,6 +9,7 @@ from pyspark.sql.functions import udf
 spark = SparkSession.builder.appName('TwitterTask').getOrCreate()
 
 file_path = 'data/tweets.json'
+output_file_path = 'output/dataframe'
 
 mentions_regex = '(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9_]+)'
 hashtag_regex = '(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9_]+)'
@@ -31,27 +32,85 @@ df_tweets = spark.read.json(file_path)
 
 # Find how many tweets each user has
 df_tweets_count_by_user = df_tweets.groupBy('user').count().sort(col('count').desc())
+df_tweets_count_by_user \
+    .repartition(1) \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/tweet_count_by_user")
 
 # Find all the persons mentioned on tweets
 df_tweets_mentions = df_tweets.withColumn('mentions', get_mentions('text'))
 df_tweets_mentions_only = df_tweets_mentions.select(F.explode(df_tweets_mentions.mentions))
+df_tweets_mentions_only \
+    .repartition(1) \
+    .write.option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/all_mentions")
 
 # Count how many times each person is mentioned
 df_tweets_mentions_only_count = df_tweets_mentions_only.groupBy('col').count().sort(col('count').desc())
+df_tweets_mentions_only_count \
+    .repartition(1) \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/mentions_count")
 
 # Find the 10 most mentioned persons
 df_tweets_mentions_only_top_ten = df_tweets_mentions_only.groupBy('col').count().sort(
     col('count').desc()).limit(10)
+df_tweets_mentions_only_top_ten \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/top_ten_mentions")
 
 # Find all the hashtags mentioned on a tweet
 df_tweets_hashtag = df_tweets.withColumn('hashtag', get_hashtag('text'))
 df_tweets_hashtag_only = df_tweets_hashtag.select(F.explode(df_tweets_hashtag.hashtag))
+df_tweets_hashtag_only \
+    .repartition(1) \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/all_hashtags")
 
 # Count how many times each hashtag is mentioned
 df_tweets_hashtag_only_count = df_tweets_hashtag_only.groupBy('col').count().sort(col('count').desc())
+df_tweets_hashtag_only_count \
+    .repartition(1) \
+    .write.option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/hashtag_count")
 
 # Find the 10 most popular Hashtags
 df_tweets_hashtag_only_top_ten = df_tweets_hashtag_only.groupBy('col').count().sort(col('count').desc()).limit(10)
+df_tweets_hashtag_only_top_ten \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/top_ten_hashtags")
 
 # Find the top 5 countries which tweet the most.
 df_tweets_by_country = df_tweets.groupBy('country').count().sort(col('count').desc()).limit(5)
+df_tweets_by_country \
+    .write.option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/dataframe/top_five_tweeting_countries")
--- rdd.py
+++ rdd.py
@@ -30,6 +30,15 @@ df_tweets = spark.read.json(file_path).rdd
 # Find how many tweets each user has
 df_tweets_count_user = df_tweets.map(lambda x: (x[4], 1)).reduceByKey(lambda x, y: x + y).sortBy(
     lambda x: -x[1])
+df_tweets_count_user \
+    .toDF(schema=['user', 'count']) \
+    .repartition(1) \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/rdd/tweet_count_by_user")
 
 # Count how many times each person is mentioned
 df_tweets_mentions = df_tweets.flatMap(lambda x: re.findall(mentions_regex, x[3]))
@@ -37,7 +46,15 @@ df_tweets_mentions = df_tweets.flatMap(lambda x: re.findall(mentions_regex, x[3]
 # Find the 10 most mentioned persons
 df_tweets_mentions_only = df_tweets_mentions.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y).sortBy(
     lambda x: -x[1])
-df_tweets_mentions_top_ten = df_tweets_mentions_only.take(10)
+df_tweets_mentions_only \
+    .toDF(schema=['mentions', 'count']) \
+    .limit(10) \
+    .repartition(1) \
+    .write.option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/rdd/top_ten_mentions")
 
 # Find all the hashtags mentioned on a tweet
 df_tweets_hashtags = df_tweets.flatMap(lambda x: re.findall(hashtag_regex, x[3]))
@@ -45,9 +62,24 @@ df_tweets_hashtags = df_tweets.flatMap(lambda x: re.findall(hashtag_regex, x[3])
 # Find the 10 most popular Hashtags
 df_tweets_hashtags_only = df_tweets_hashtags.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y).sortBy(
     lambda x: -x[1])
-df_tweets_hashtags_top_ten = df_tweets_hashtags_only.take(10)
+df_tweets_hashtags_only \
+    .toDF(schema=['mentions', 'count']) \
+    .limit(10) \
+    .repartition(1) \
+    .write.option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/rdd/top_ten_mentions")
 
 # Find the top 5 countries which tweet the most.
 df_tweets_by_country = df_tweets.map(lambda x: (x[0], 1)).reduceByKey(lambda x, y: x + y).sortBy(
     lambda x: -x[1])
-print(df_tweets_by_country.take(5))
+df_tweets_by_country \
+    .toDF(schema=['countries', 'count']) \
+    .write \
+    .option("header", "true") \
+    .option("sep", ",") \
+    .format("com.databricks.spark.csv") \
+    .mode("overwrite") \
+    .csv("output/rdd/top_five_tweeting_countries")
